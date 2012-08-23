dd857516e6a1f8554fb4d0f7c1fb505cb5f3aea9
maestrofjp <peter@mach-ii.com>
2012-08-22 21:23:42 -0500

    - Added Organization and Address domain classes

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -6,6 +6,9 @@ class BootStrap {
     def init = { servletContext ->
 		// Setup some DB defaults
 		if (GrailsUtil.environment == "development") {
+			// Setting
+			new Organization(organizationName: 'Cool Project', organizationAddress: '123 Main Street').save()
+			
 			// Statuses
 			new Status(status: 'Active').save()
 			new Status(status: 'Archived').save()
