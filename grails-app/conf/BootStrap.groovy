ace77630742adfbcb1d311efd8b3d464e466f70f
maestrofjp <peter@mach-ii.com>
2012-08-22 20:49:32 -0500

    - Added basic roles to BootStrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -6,8 +6,13 @@ class BootStrap {
     def init = { servletContext ->
 		// Setup some DB defaults
 		if (GrailsUtil.environment == "development") {
+			// Statuses
 			new Status(status: 'Active').save()
 			new Status(status: 'Archived').save()
+			
+			// Roles
+			new Role(role: 'Admin').save()
+			new Role(role: 'User').save()
 		}
     }
     def destroy = {
