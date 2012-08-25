d6ab80c08646e9842eb27b0b87fc0387cdf44701
maestrofjp <peter@mach-ii.com>
2012-08-25 14:34:36 -0500

    - Fixed controller for Activity to use TWBootstrap

--- grails-app/controllers/enlist/grails/ActivityController.groovy
+++ grails-app/controllers/enlist/grails/ActivityController.groovy
@@ -1,106 +1,110 @@
 package enlist.grails
 
+
 import org.springframework.dao.DataIntegrityViolationException
-import grails.converters.JSON
-import static javax.servlet.http.HttpServletResponse.*
 
 class ActivityController {
 
-    static final int SC_UNPROCESSABLE_ENTITY = 422
+    static allowedMethods = [create: ['GET', 'POST'], edit: ['GET', 'POST'], delete: 'POST']
 
-    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
-
-    def index() { }
+    def index() {
+        redirect action: 'list', params: params
+    }
 
     def list() {
         params.max = Math.min(params.max ? params.int('max') : 10, 100)
-		response.setIntHeader('X-Pagination-Total', Activity.count())
-		render Activity.list(params) as JSON
+        [activityInstanceList: Activity.list(params), activityInstanceTotal: Activity.count()]
     }
 
-    def save() {
-        def activityInstance = new Activity(request.JSON)
-        def responseJson = [:]
-        if (activityInstance.save(flush: true)) {
-            response.status = SC_CREATED
-            responseJson.id = activityInstance.id
-            responseJson.message = message(code: 'default.created.message', args: [message(code: 'activity.label', default: 'Activity'), activityInstance.id])
-        } else {
-            response.status = SC_UNPROCESSABLE_ENTITY
-            responseJson.errors = activityInstance.errors.fieldErrors.collectEntries {
-                [(it.field): message(error: it)]
-            }
-        }
-        render responseJson as JSON
-    }
+    def create() {
+		switch (request.method) {
+		case 'GET':
+        	[activityInstance: new Activity(params)]
+			break
+		case 'POST':
+	        def activityInstance = new Activity(params)
+	        if (!activityInstance.save(flush: true)) {
+	            render view: 'create', model: [activityInstance: activityInstance]
+	            return
+	        }
 
-    def get() {
-        def activityInstance = Activity.get(params.id)
-        if (activityInstance) {
-			render activityInstance as JSON
-        } else {
-			notFound params.id
+			flash.message = message(code: 'default.created.message', args: [message(code: 'activity.label', default: 'Activity'), activityInstance.id])
+	        redirect action: 'show', id: activityInstance.id
+			break
 		}
     }
 
-    def update() {
+    def show() {
         def activityInstance = Activity.get(params.id)
         if (!activityInstance) {
-            notFound params.id
+			flash.message = message(code: 'default.not.found.message', args: [message(code: 'activity.label', default: 'Activity'), params.id])
+            redirect action: 'list'
             return
         }
 
-        def responseJson = [:]
+        [activityInstance: activityInstance]
+    }
 
-        if (request.JSON.version != null) {
-            if (activityInstance.version > request.JSON.version) {
-				response.status = SC_CONFLICT
-				responseJson.message = message(code: 'default.optimistic.locking.failure',
-						args: [message(code: 'activity.label', default: 'Activity')],
-						default: 'Another user has updated this Activity while you were editing')
-				cache false
-				render responseJson as JSON
-				return
-            }
-        }
+    def edit() {
+		switch (request.method) {
+		case 'GET':
+	        def activityInstance = Activity.get(params.id)
+	        if (!activityInstance) {
+	            flash.message = message(code: 'default.not.found.message', args: [message(code: 'activity.label', default: 'Activity'), params.id])
+	            redirect action: 'list'
+	            return
+	        }
 
-        activityInstance.properties = request.JSON
+	        [activityInstance: activityInstance]
+			break
+		case 'POST':
+	        def activityInstance = Activity.get(params.id)
+	        if (!activityInstance) {
+	            flash.message = message(code: 'default.not.found.message', args: [message(code: 'activity.label', default: 'Activity'), params.id])
+	            redirect action: 'list'
+	            return
+	        }
 
-        if (activityInstance.save(flush: true)) {
-            response.status = SC_OK
-            responseJson.id = activityInstance.id
-            responseJson.message = message(code: 'default.updated.message', args: [message(code: 'activity.label', default: 'Activity'), activityInstance.id])
-        } else {
-            response.status = SC_UNPROCESSABLE_ENTITY
-            responseJson.errors = activityInstance.errors.fieldErrors.collectEntries {
-                [(it.field): message(error: it)]
-            }
-        }
+	        if (params.version) {
+	            def version = params.version.toLong()
+	            if (activityInstance.version > version) {
+	                activityInstance.errors.rejectValue('version', 'default.optimistic.locking.failure',
+	                          [message(code: 'activity.label', default: 'Activity')] as Object[],
+	                          "Another user has updated this Activity while you were editing")
+	                render view: 'edit', model: [activityInstance: activityInstance]
+	                return
+	            }
+	        }
 
-        render responseJson as JSON
+	        activityInstance.properties = params
+
+	        if (!activityInstance.save(flush: true)) {
+	            render view: 'edit', model: [activityInstance: activityInstance]
+	            return
+	        }
+
+			flash.message = message(code: 'default.updated.message', args: [message(code: 'activity.label', default: 'Activity'), activityInstance.id])
+	        redirect action: 'show', id: activityInstance.id
+			break
+		}
     }
 
     def delete() {
         def activityInstance = Activity.get(params.id)
         if (!activityInstance) {
-            notFound params.id
+			flash.message = message(code: 'default.not.found.message', args: [message(code: 'activity.label', default: 'Activity'), params.id])
+            redirect action: 'list'
             return
         }
 
-        def responseJson = [:]
         try {
             activityInstance.delete(flush: true)
-            responseJson.message = message(code: 'default.deleted.message', args: [message(code: 'activity.label', default: 'Activity'), params.id])
-        } catch (DataIntegrityViolationException e) {
-            response.status = SC_CONFLICT
-            responseJson.message = message(code: 'default.not.deleted.message', args: [message(code: 'activity.label', default: 'Activity'), params.id])
+			flash.message = message(code: 'default.deleted.message', args: [message(code: 'activity.label', default: 'Activity'), params.id])
+            redirect action: 'list'
         }
-        render responseJson as JSON
-    }
-
-    private void notFound(id) {
-        response.status = SC_NOT_FOUND
-        def responseJson = [message: message(code: 'default.not.found.message', args: [message(code: 'activity.label', default: 'Activity'), params.id])]
-        render responseJson as JSON
+        catch (DataIntegrityViolationException e) {
+			flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'activity.label', default: 'Activity'), params.id])
+            redirect action: 'show', id: params.id
+        }
     }
 }
