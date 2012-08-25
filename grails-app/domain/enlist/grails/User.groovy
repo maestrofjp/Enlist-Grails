f9586bcbff2d9583159bdfe29240dfe5ec732b7d
Kurt Wiersma <kwiersma@gmail.com>
2012-08-25 16:50:08 -0500

    Started to setup the Spring security attributes

--- grails-app/domain/enlist/grails/User.groovy
+++ grails-app/domain/enlist/grails/User.groovy
@@ -1,22 +1,57 @@
 package enlist.grails
 
 class User {
-	
-	String firstName
+
+    transient springSecurityService     //SpringSecurityService
+
+    String firstName
 	String lastName
 	String email
-	String twitterUsername
-	String identicaUsername
+	//String twitterUsername
+	//String identicaUsername
 	String phone
 	Address address
 	Chapter chapter
 	Status status
-	Role role
+	//Role role
+
+    // Spring Security Stuff
+    String username
+    String password
+    boolean enabled
+    boolean accountExpired
+    boolean accountLocked
+    boolean passwordExpired
 
     static constraints = {
-    	firstName(blank:false)
-    	lastName(blank:false)
-    	email(email:true, unique:true)
+        username blank: false, unique: true
+        password blank: false
+
+        firstName(blank:false)
+        lastName(blank:false)
+        email(email:true, unique:true)
+    }
+
+    static mapping = {
+        password column: '`password`'
+    }
+
+    Set<Role> getAuthorities() {
+        UserRole.findAllBySecUser(this).collect { it.Role } as Set
+    }
+
+    def beforeInsert() {
+        encodePassword()
+    }
+
+    def beforeUpdate() {
+        if (isDirty('password')) {
+            encodePassword()
+        }
+    }
+
+    protected void encodePassword() {
+        password = springSecurityService.encodePassword(password)
     }
 
     String toString() {
